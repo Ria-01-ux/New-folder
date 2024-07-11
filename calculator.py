79671a9ff3daacd9f3ce65513fc901c895a8cebe
Ria <bhardwajria7@gmail.com>
2024-07-11 15:56:33 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,35 @@
+#Simple calculator using 
+def add(x,y):
+    return x+ypython
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    if y != 0:
+        return x/y
+    else:
+        return "Error! Division by zero."
+    def calculator():
+        print("Select operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        choice = input("Enter your choice(1/2/3/4): ")
+        if choice in ['1','2','3','4']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            if choice == '1':
+                print(f"{num1} + {num2} = {add(num1, num2)}")
+            elif choice == '2':
+                print(f"{num1} - {num2} = {subtract(num1, num2)}")
+            elif choice == '3':
+                print(f"{num1} * {num2} = {multiply(num1, num2)}")
+            elif choice == '4':
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
+        else:
+            print("Invalid Input")
+
+    if _name__ == "_main_":
+        calculator()
